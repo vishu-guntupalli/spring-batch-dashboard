1722e1d20275c9c2df2f9257deaecfd201b54aa6
Guntupalli, Vishu <viswanath.guntupalli@vanderbilt.edu>
2017-02-21 10:25:29 -0600

    - Refactoring code.

--- spring_batch_dashboard/dashboard/views.py
+++ spring_batch_dashboard/dashboard/views.py
@@ -11,9 +11,10 @@ __batch_exec_sql__ = 'select bi.job_name,bi.job_instance_id,be.start_time,be.end
                    'inner join batch_step_execution se on (be.job_execution_id=se.job_execution_id)  ' \
                    'order by bi.job_name, bi.job_instance_id'
 
-__batch_resultset__ = ('jobName','jobInstanceId','jobStartTime','jobEndTime','jobStatus','keyName','keyDateValue','stepName',
+__batch_result_set__ = ('jobName', 'jobInstanceId', 'jobStartTime', 'jobEndTime', 'jobStatus', 'keyName', 'keyDateValue', 'stepName',
                        'stepStartTime','stepEndTime','stepStatus','stepReadCount','stepWriteCount','stepFilterCount',
                        'stepCommitCount')
+
 
 def dashboard(request):
     results = []
@@ -23,7 +24,7 @@ def dashboard(request):
         rows = cursor.fetchall()
 
     for result_2 in rows:
-        rowsDict = dict(zip(__batch_resultset__,result_2))
+        rowsDict = dict(zip(__batch_result_set__, result_2))
         results.append(rowsDict)
 
     for result_1 in results:
@@ -45,26 +46,35 @@ def dashboard(request):
                             if jobInstanceRow['jobInstanceId'] == result_2['jobInstanceId']:
                                 job_instance_present = True
                     if not job_instance_present:
-                        job_instance['jobInstanceId'] = result_2['jobInstanceId']
-                        job_instance['jobStartTime'] = result_2['jobStartTime']
-                        job_instance['jobEndTime'] = result_2['jobEndTime']
-                        job_instance['jobStatus'] = result_2['jobStatus']
-                        job_instance['keyName'] = result_2['keyName']
-                        job_instance['keyDateValue'] = result_2['keyDateValue']
-                        job_instance['jobSteps'] = []
+                        build_job_instance(job_instance, result_2)
                         for result_3 in results:
                             if (result_3['jobName'] == result_2['jobName']) and (
                                         result_2['jobInstanceId'] == result_3['jobInstanceId']):
-                                job_step = {}
-                                job_step['stepName'] = result_3['stepName']
-                                job_step['stepStartTime'] = result_3['stepStartTime']
-                                job_step['stepEndTime'] = result_3['stepEndTime']
-                                job_step['stepStatus'] = result_3['stepStatus']
-                                job_step['stepReadCount'] = result_3['stepReadCount']
-                                job_step['stepWriteCount'] = result_3['stepWriteCount']
-                                job_step['stepFilterCount'] = result_3['stepFilterCount']
-                                job_step['stepCommitCount'] = result_3['stepCommitCount']
+                                job_step = build_job_step(result_3)
                                 job_instance['jobSteps'].append(job_step)
                         job_object['jobInstances'].append(job_instance)
             formatted_results.append(job_object)
-    return render(request, 'templates/index.html', {'all_jobs': formatted_results})
+    return render(request, 'templates/dashboard.html', {'all_jobs': formatted_results})
+
+
+def build_job_step(result_3):
+    job_step = {}
+    job_step['stepName'] = result_3['stepName']
+    job_step['stepStartTime'] = result_3['stepStartTime']
+    job_step['stepEndTime'] = result_3['stepEndTime']
+    job_step['stepStatus'] = result_3['stepStatus']
+    job_step['stepReadCount'] = result_3['stepReadCount']
+    job_step['stepWriteCount'] = result_3['stepWriteCount']
+    job_step['stepFilterCount'] = result_3['stepFilterCount']
+    job_step['stepCommitCount'] = result_3['stepCommitCount']
+    return job_step
+
+
+def build_job_instance(job_instance, result_2):
+    job_instance['jobInstanceId'] = result_2['jobInstanceId']
+    job_instance['jobStartTime'] = result_2['jobStartTime']
+    job_instance['jobEndTime'] = result_2['jobEndTime']
+    job_instance['jobStatus'] = result_2['jobStatus']
+    job_instance['keyName'] = result_2['keyName']
+    job_instance['keyDateValue'] = result_2['keyDateValue']
+    job_instance['jobSteps'] = []
