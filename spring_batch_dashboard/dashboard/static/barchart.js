22a97e28a84b88520c5b3d3d820469cbc9fbe543
Vishu <iamno.16@gmail.com>
2017-02-26 19:37:26 -0600

    - Adding div for adding piechart. - Adding JS file for piechart creation.

--- spring_batch_dashboard/dashboard/static/barchart.js
+++ spring_batch_dashboard/dashboard/static/barchart.js
@@ -1,7 +1,7 @@
 /**
  * Created by guntupv on 2/25/17.
  */
-var svg = d3.select("svg"),
+var svg = d3.select("#barg").select("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
---
+++ spring_batch_dashboard/dashboard/static/piechart.js
@@ -0,0 +1,50 @@
+/**
+ * Created by guntupv on 2/26/17.
+ */
+
+var width = 250,
+    height = 250,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var labelArc = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg = d3.select("#pieg").select("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data.csv", type, function(error, data) {
+  if (error) throw error;
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+      .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.age); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.age; });
+});
+
+function type(d) {
+  d.population = +d.population;
+  return d;
+}
